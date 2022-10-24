5d06b2e65e22f642505f94961076ca040b7ebedf
littlelee2022 <eun0lee@gmail.com>
2022-10-24 15:17:44 +0900

    first commit

---
+++ demoList.py
@@ -0,0 +1,17 @@
+#demoList.py
+
+strA = "python is very powerful"
+
+print(strA[0:6])
+print(strA[:6])
+print(strA[-3:])
+
+lst = [10,20,30,40,50]
+print(lst[:2])
+print(lst[-2:])
+
+#디버깅 환경 세팅
+#중단점을 지정(디버깅시에 중단)
+for i in lst:
+    print(i)
+
